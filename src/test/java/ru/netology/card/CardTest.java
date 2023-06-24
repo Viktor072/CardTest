bde81ee55619653115f26a65757da2d967366d5f
Viktor <rodkiv@mail.ru>
2023-06-24 14:49:58 +0300

    Test1

--- src/test/java/ru/netology/card/CardTest.java
+++ src/test/java/ru/netology/card/CardTest.java
@@ -1,6 +1,5 @@
 package ru.netology.card;
 
-import org.apache.commons.lang3.SystemUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,10 +19,7 @@ public class CardTest {
 
     @BeforeAll
     static void setUpAll() {
-        //System.setProperty("web-driver.chrome.driver", "driver/mac/chromedriver");
-        if (SystemUtils.IS_OS_WINDOWS)
-            System.setProperty("webdriver.chrome.driver", "artifacts/chromedriver.exe");
-        else System.setProperty("webdriver.chrome.driver", "artifacts/chromedriver_linux");
+        System.setProperty("web-driver.chrome.driver", "driver/windows/chromedriver");
     }
 
     @BeforeEach
@@ -45,8 +41,8 @@ public class CardTest {
     @Test
     void shouldValidReq () {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79004442233");
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Чибисова Регина");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79034567890");
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.cssSelector("[type=\"button\"]")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id]")).getText();
